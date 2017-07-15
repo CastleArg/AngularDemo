aeef86eb0546a78a1cb19d2d78922fd2735f72c5
castleArg <markchurcher@gmail.com>
2017-07-16 11:46:39 +1200

    display the time on products

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from "./product.model";
+import { CurrencyPipe } from '@angular/common'
 
 @Component({
   selector: 'app-product',
@@ -7,10 +8,26 @@ import { Product } from "./product.model";
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
- @Input() product: Product;
-  constructor() { }
+  @Input() product: Product;
+  public dateString;
 
   ngOnInit() {
+    this.dateString = this.getDateString()
   }
 
+  getDateString() {
+    let now = new Date();
+    let productDate = new Date(this.product.date);
+    let daysElapsed = this.calculateDaysElapsed(now, productDate);
+
+    return daysElapsed <= 7 ? `${daysElapsed} days ago` : new Date(this.product.date).toLocaleDateString();
+  }
+
+  calculateDaysElapsed(a: Date, b: Date): number {
+    var dayMs = 1000 * 60 * 60 * 24;
+    var aMs = a.getTime();
+    var bMs = b.getTime();
+    var differenceMs = aMs - bMs;
+    return Math.round(differenceMs / dayMs);
+  }
 }

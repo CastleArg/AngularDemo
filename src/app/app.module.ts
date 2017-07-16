ffecdb2a44bca47f5c88bbc269e879c159652191
castleArg <markchurcher@gmail.com>
2017-07-16 14:59:36 +1200

    floating header in its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductComponent } from './product/product.component';
 import { AdComponent } from './ad/ad.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
     ProductComponent,
-    AdComponent
+    AdComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
